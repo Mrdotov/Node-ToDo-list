85ebf9b72e9dad09cffb07bf064ec69633c0a16c
Mogan <106959238+Mrdotov@users.noreply.github.com>
2023-05-24 20:49:47 +0300

    Third commit

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const path = require('path')
 
-const app = express;
+
+const app = express();
 
 const PORT = 3000;
 
- app.set('view engine', 'ejs')
+app.set('view engine', 'ejs')
+
+const createPath = (page) => path.resolve(__dirname,'views',`${page}.html`)
+
+app.listen(PORT, (error) => {
+    error ? console.log(error) : console.log(`listening port ${PORT}`);
+  }); 
+
+ app.get('/',(req,res)=>{
+    const title = 'TODOS'
+res.sendFile(createPath('index'))
+    res.status(200)
+ })
+
+app.use(express.static('styles'));
