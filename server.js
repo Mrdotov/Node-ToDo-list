281d4c3eb3de355ad6571da5c9f2e7c7e0da86e6
Mogan <106959238+Mrdotov@users.noreply.github.com>
2023-05-26 15:27:49 +0300

    save commit

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const path = require('path')
 
 const Post = require('./post');
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const app = express();
 
 const PORT = 3000;
 
-const db = 'mongodb+srv://otovmid:12qwerty1A@cluster0.73suz04.mongodb.net/ToDo-list?retryWrites=true&w=majority'
+// const db = 'mongodb+srv://otovmid:12qwerty1A@cluster0.73suz04.mongodb.net/ToDo-list?retryWrites=true&w=majority'
 
-mongoose
-.connect(db,{useNewUrlParser: true,useUnifiedTopology:true})
-.then((res) => console.log('Connected to the database'))
-.catch((error)=> console.log(error))
+// mongoose
+// .connect(db,{useNewUrlParser: true,useUnifiedTopology:true})
+// .then((res) => console.log('Connected to the database'))
+// .catch((error)=> console.log(error))
 
 app.set('view engine', 'ejs')
 
